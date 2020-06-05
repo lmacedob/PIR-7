cd7a8e7b2f5847290a73ea092ad252962c388f8c
Luis Macedo <60890000+lmacedob@users.noreply.github.com>
2020-06-05 11:49:37 +0200

    Vendredi 4 juin avance

--- Code/test_parser.py
+++ Code/test_parser.py
@@ -4,30 +4,39 @@ import outilParser
 
 if __name__ == "__main__":
     try:
-        file = sys.argv[1]      #source file
-        keywords = sys.argv[2:]  #keywords necessary to do the text treatment
+        file_1 = sys.argv[1]      #source file 1
+        file_2 = sys.argv[2]      #source file 2
+        keywords = sys.argv[3:]  #keywords necessary to do the text treatment
+
+        if ".log" not in (file_1 and file_2):
+            raise IndexError
     except IndexError:
-        raise SystemExit("******ERROR : Parameters missing******\n Usage : {sysargv[1]} file to treat ;  {sys.argv[2:]} keywords")
+        raise SystemExit("******ERROR : Parameters missing******\n Usage : {sysargv[1,2]} files to treat (.log) ;  {sys.argv[3:]} keywords")
 
     #print("\n")
     print("Keywords: {0}".format(keywords))
-    #print("\n")
+    print("\n")
 
+    ##########Extraction of lines
     #Let's get the lines from the log that interest us,
     #using the keyword to extract only the lines containing it
     #As a result, we have a list containing the interesting lines
-    lines = outilParser.select_lines(file, keywords)
+    lines = outilParser.select_lines(file_1, keywords)
+    lines.append(outilParser.select_lines(file_2,keywords))
 
-    #Latency test
+    ###########Latency test
     lat_test = outilParser.get_latency(str(lines[1]),str(lines[-1]))
     #print(outilParser.get_time(str(lines[0])))
-    print("Latency test between \n  -----> {0} \n  and \n  -----> {1} \n  = {2} s".format(str(lines[0]),str(lines[-1]),lat_test))
-    print("\n")
+    #print("Latency test between \n  -----> {0} \n  and \n  -----> {1} \n  = {2} s".format(str(lines[0]),str(lines[-1]),lat_test))
+    #print("\n")
+
+
+
 
     #We convert that list of lines into a string
     lines_text = outilParser.toString(lines, keywords)
     print(lines_text)  #print for debugging
-    print("\n")
+    #print("\n")
 
     #We output the total number of lines only, and not all the content
     #print(outilParser.showOnly_nbLines(lines, keywords))
